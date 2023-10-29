245369efea88699cb8df593987c4d4671c8ec612
amy-enn <122310783+amy-enn@users.noreply.github.com>
2023-10-29 12:03:40 -0300

    reduced files into one single app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,46 @@
 import { useState, useEffect } from 'react';
-import { generateBoggleBoard } from './Boggle';
 import './App.css';
+
+const dice: string[] = [
+  "RIFOBX",
+  "IYDEVL",
+  "ANEDVZ",
+  "PAMBJO",
+  "GUELNW",
+  "EOTISL",
+  "UTOKND",
+  "HYAGBR",
+  "EHLRST",
+  "ACITOA",
+  "SORLND",
+  "CEMOTU",
+  "AILSUY",
+  "LRVERH",
+  "WIDQUK",
+  "NSEEHG",
+];
+
+
+const generateBoggleBoard = (): string[][] => {
+
+  
+  const boggleBoard: string[][] = [];
+  for (let row = 0; row < 4; row++) {
+      const rowLetters: string[] = [];
+      for (let col = 0; col < 4; col++) {
+          const dieIndex = Math.floor(Math.random() * dice.length);
+          const die = dice[dieIndex];
+          const letterIndex = Math.floor(Math.random() * die.length);
+          const letter = die[letterIndex];
+          rowLetters.push(letter);
+      }
+      boggleBoard.push(rowLetters);
+  }
+
+  console.log(boggleBoard);
+  return (boggleBoard);
+}
+
 
 function App() {
   const [boggleBoard, setBoggleBoard] = useState<string[][] | null>(null);
